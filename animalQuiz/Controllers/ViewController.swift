2783bcadba8b55d72118dabecfabde43b5a51d77
Aidar <audars@mail.ru>
2023-02-05 01:06:44 +0600

    init

---
+++ animalQuiz/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  animalQuiz
+//
+//  Created by Айдар Нуркин on 05.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
